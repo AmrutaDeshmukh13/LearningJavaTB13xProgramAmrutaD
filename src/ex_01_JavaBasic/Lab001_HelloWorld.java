a8327ea12ac3deac72899edc71ec5d73b9a8647e
Amruta Vikram Deshmukh <amrujagtap@gmail.com>
2025-06-06 20:20:09 +0530

    First java projet

---
+++ src/ex_01_JavaBasic/Lab001_HelloWorld.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasic;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
---
+++ src/ex_01_JavaBasic/Lab002_HW_Extra.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasic;
+
+public class Lab002_HW_Extra {
+    public static void main(String[] args) {
+        System.out.print(2+2);
+    }
+}
