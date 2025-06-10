c8d3dd8d455daf14bb44a5c50346f37c606bcb3d
Amruta Vikram Deshmukh <amrujagtap@gmail.com>
2025-06-10 22:19:24 +0530

    Print 9 table

--- src/ex_02_JavaBasic/Lab001_PrintF9Table.java
+++ src/ex_02_JavaBasic/Lab001_PrintF9Table.java
@@ -2,16 +2,16 @@ package ex_02_JavaBasic;
 
 public class Lab001_PrintF9Table {
     public static void main(String[] args) {
-        int num = 9;
-        System.out.printf("%d x 1 = %d%n", num, num * 1);
-        System.out.printf("%d x 2 = %d%n", num, num * 2);
-        System.out.printf("%d x 3 = %d%n", num, num * 3);
-        System.out.printf("%d x 4 = %d%n", num, num * 4);
-        System.out.printf("%d x 5 = %d%n", num, num * 5);
-        System.out.printf("%d x 6 = %d%n", num, num * 6);
-        System.out.printf("%d x 7 = %d%n", num, num * 7);
-        System.out.printf("%d x 8 = %d%n", num, num * 8);
-        System.out.printf("%d x 9 = %d%n", num, num * 9);
-        System.out.printf("%d x 10 = %d%n", num, num * 10);
+        int number = 9;
+        System.out.printf("%d x 1 = %d%n", number, number * 1);
+        System.out.printf("%d x 2 = %d%n", number, number * 2);
+        System.out.printf("%d x 3 = %d%n", number, number * 3);
+        System.out.printf("%d x 4 = %d%n", number, number * 4);
+        System.out.printf("%d x 5 = %d%n", number, number * 5);
+        System.out.printf("%d x 6 = %d%n", number, number * 6);
+        System.out.printf("%d x 7 = %d%n", number, number * 7);
+        System.out.printf("%d x 8 = %d%n", number, number * 8);
+        System.out.printf("%d x 9 = %d%n", number, number * 9);
+        System.out.printf("%d x 10 = %d%n", number, number * 10);
     }
 }
