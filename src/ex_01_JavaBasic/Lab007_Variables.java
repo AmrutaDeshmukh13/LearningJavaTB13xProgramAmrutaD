7925eac16b941c6b93fbe08b00808c2cb84443e9
Amruta Vikram Deshmukh <amrujagtap@gmail.com>
2025-06-10 22:15:42 +0530

    Variable types,final

---
+++ src/ex_01_JavaBasic/Lab007_Variables.java
@@ -0,0 +1,8 @@
+package ex_01_JavaBasic;
+
+public class Lab007_Variables {
+    public static void main(String[] args) {
+      //  datatype variable name=value
+        byte b=10;
+    }
+}
---
+++ src/ex_02_JavaBasic/LabExample1.java
@@ -0,0 +1,19 @@
+package ex_02_JavaBasic;
+
+public class LabExample1 {
+    //
+//    int i;
+//    static int s;
+//
+
+    public static void main(String[] args) {
+        int a = 10;
+        {
+            int b = 10;
+            System.out.println(b);
+        }
+        int b = 90;
+        System.out.println(b);
+
+    }
+}
