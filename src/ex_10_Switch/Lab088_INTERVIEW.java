15d252eccce92a3482d16327832121808ba746a1
Amruta Vikram Deshmukh <amrujagtap@gmail.com>
2025-06-16 22:37:08 +0530

    16thJuneTask

---
+++ src/ex_10_Switch/Lab088_INTERVIEW.java
@@ -0,0 +1,10 @@
+package ex_10_Switch;
+
+public class Lab088_INTERVIEW {
+    public static void main(String[] args) {
+        int a = 10;
+        switch (a) {
+
+        }
+    }
+}
---
+++ src/ex_10_Switch/Lab090_Interview.java
@@ -0,0 +1,10 @@
+package ex_10_Switch;
+
+public class Lab090_Interview {
+
+    public static void main(String[] args) {
+//        switch (true){  //not possible boolean value can not pass
+//
+//        }
+    }
+}
