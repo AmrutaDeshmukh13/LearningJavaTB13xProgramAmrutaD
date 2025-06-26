049e8ac187209412e45294fb179beb8e9746afb7
Amruta <amrujagtap@gmail.com>
2025-06-26 20:55:05 +0530

    First String coding challenage

---
+++ src/ex_15_Strings/CodingChallenage1.java
@@ -0,0 +1,11 @@
+package ex_15_Strings;
+
+public class CodingChallenage1 {
+    public static void main(String[] args) {
+        String s="Hello World";
+        int len=s.length();
+        char c=s.charAt(0);
+        String part= s.substring(6,11);
+        System.out.print("Length: "+len +" First char: "+ c+ " Substring: "+part);
+    }
+}
