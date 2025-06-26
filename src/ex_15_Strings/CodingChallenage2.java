e4085c9173e8e78d5b651c179a0b1becaf9b9f8c
Amruta <amrujagtap@gmail.com>
2025-06-26 21:16:42 +0530

    Second String coding challenage

---
+++ src/ex_15_Strings/CodingChallenage2.java
@@ -0,0 +1,12 @@
+package ex_15_Strings;
+
+public class CodingChallenage2 {
+    public static void main(String[] args) {
+        String string1="Hello";
+        String string2="hello";
+        String string3="Hello";
+        System.out.println("equls: "+string1.equals(string2)+", equalsIgnoreCase(): "+string1.equalsIgnoreCase(string2)+", compareTo(): " +string1.compareTo(string2));
+
+
+    }
+}
