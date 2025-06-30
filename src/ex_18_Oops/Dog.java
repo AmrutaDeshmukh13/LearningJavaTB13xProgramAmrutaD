02048a4da8b39c7d13bccd2caeb85a21d81b5602
Amruta <amrujagtap@gmail.com>
2025-06-30 11:02:56 +0530

    30thJune

---
+++ src/ex_18_Oops/Dog.java
@@ -0,0 +1,15 @@
+package ex_18_Oops;
+
+public class Dog {
+    String name;
+    String breed;
+    String color_eyes;
+
+
+    void walk() {
+    }
+
+    void bark() {
+    }
+
+}
---
+++ src/ex_18_Oops/Lab166_OOPs.java
@@ -0,0 +1,10 @@
+package ex_18_Oops;
+
+public class Lab166_OOPs {
+    public static void main(String[] args) {
+
+        Person amit = new Person();
+        Person pramod = new Person();
+
+    }
+}
