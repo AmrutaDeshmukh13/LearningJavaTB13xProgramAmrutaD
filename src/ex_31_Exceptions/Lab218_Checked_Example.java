579b5c0d8d0c623be2bbb31c17f9f028d7c113ea
Amruta <amrujagtap@gmail.com>
2025-07-12 23:16:20 +0530

    Exception,Collection Framework

---
+++ src/ex_31_Exceptions/Lab218_Checked_Example.java
@@ -0,0 +1,8 @@
+package ex_31_Exceptions;
+import java.io.FileInputStream;
+public class Lab218_Checked_Example {
+    public static void main(String[] args) {
+        //  Checked exception
+        // FileInputStream fileInputStream = new FileInputStream("C://a.txt");
+    }
+}
---
+++ src/ex_33_Collection_Framework_DSA/LIST/Lab241_LIST_I.java
@@ -0,0 +1,14 @@
+package ex_33_Collection_Framework_DSA.LIST;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab241_LIST_I {
+    public static void main(String[] args) {
+//        List l = new ArrayList(); // Dynamic Diapatch
+        ArrayList l = new ArrayList(); // Dynamic Diapatch
+        l.add("hello");
+        l.add(123);
+        l.add(true);
+        l.add("Blah Blah");
+    }
+}
